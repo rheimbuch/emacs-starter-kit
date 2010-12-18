f85cc4b0eb7ea12ee8181a7b159e69b73493f09d
Ryan Heimbuch <rheimbuch@gmail.com>
2010-12-18 14:14:51 -0700

    Ensure menu-bar is displayed. Added C-x C-m & C-c C-m as alternate bindings for M-x

--- ryan/load.el
+++ ryan/load.el
@@ -1,2 +1,7 @@
 (require 'undo-tree)
 (global-undo-tree-mode)
+
+(if (fboundp 'menu-bar-mode) (menu-bar-mode 1))
+
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
