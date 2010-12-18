431d2e498677e15c3632dd70b91b9710a07c207f
Ryan Heimbuch <rheimbuch@gmail.com>
2010-12-18 14:16:39 -0700

    updating org-mode config - added directory for mobile-org publishing - set icalender export to use UTC time

--- ryan/org-mode-config.el
+++ ryan/org-mode-config.el
@@ -12,11 +12,16 @@
 (setq org-directory "~/Dropbox/notes/")
 (setq org-default-notes-file (concat org-directory "notes.org"))
 (setq org-startup-indented t)
+(setq org-mobile-directory "~/Dropbox/MobileOrg/")
+(setq org-mobile-inbox-for-pull (concat org-mobile-directory "mobileorg.org"))
 
 (require 'org-latex)
 (require 'org-bibtex)
 (require 'org-exp-bibtex)
 
+
+;; Export ical format using UTC time
+(setq org-icalendar-use-UTC-date-time t)
 
 ;; Handle Bibtex processing on pdf export
 (setq org-latex-to-pdf-process
