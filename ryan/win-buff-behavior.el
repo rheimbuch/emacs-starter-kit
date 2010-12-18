f4ee8a0ab36efd1e0ff942c1ce9b6c5cc27b96cb
Ryan Heimbuch <rheimbuch@gmail.com>
2010-12-18 14:17:51 -0700

    added windows.el and revive.el for persisting window layouts between sessions

--- ryan/win-buff-behavior.el
+++ ryan/win-buff-behavior.el
@@ -5,3 +5,12 @@
 
 (when (fboundp 'winner-mode)
       (winner-mode 1))
+
+(add-to-list 'load-path (concat dotfiles-dir "ryan/windows"))
+(require 'windows)
+(win:startup-with-window)
+(define-key ctl-x-map "C" 'see-you-again)
+
+(autoload 'save-current-configuration "revive" "Save status" t)
+(autoload 'resume "revive" "Resume Emacs" t)
+(autoload 'wipe "revive" "Wipe Emacs" t)
